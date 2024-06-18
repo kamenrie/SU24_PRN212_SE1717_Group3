693082c56a45607f37a05770d39e01051082113e
QuiNH <qn10782@gamil.com>
2024-06-18 15:20:55 +0700

    Cart

---
+++ SU24_PRN212_SE1717_Group3/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SU24_PRN212_SE1717_Group3.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
