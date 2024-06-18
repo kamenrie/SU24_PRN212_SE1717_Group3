3f1c67bc3207a569c40710190e48a238d084cb56
Duy Le <leduy1159@gmail.com>
2024-06-18 15:02:30 +0700

    Update ProductDAO

--- DataAccessLayer/DAO/ProductDAO.cs
+++ DataAccessLayer/DAO/ProductDAO.cs
@@ -8,92 +8,111 @@ using Utilites;
 
 namespace SU24_PRN212_SE1717_Group3.DAO
 {
-    public class ProductDAO(ApplicationDbContext dbContext)
-    {
-        public async Task<List<Product>> getAllProduct()
-        {
-           
-            return await dbContext.Product.Include(x => x.Category).Include(x=> x.Stock).ToListAsync();
-        }
+	public class ProductDAO(ApplicationDbContext dbContext)
+	{
+		public async Task<List<Product>> GetAllProduct()
+		{
+			var listProduct = await dbContext.Product.Include(x => x.Category).Include(x => x.Stock)
+				.OrderByDescending(x => x.Id).ToListAsync();
 
-        public async Task<List<Category>> getCategory()
-        {
-            return await dbContext.Category.ToListAsync();
-        }
+			listProduct.ForEach(product => { product.Image = ImgUtil.Decompress(product.Image); });
+			return listProduct;
+		}
 
-        public async Task<List<Shop>> getShop()
-        {
-            return await dbContext.Shop.ToListAsync();
-        }
+		public async Task<List<Product>> GetAllProductBySearchName(string name)
+		{
+			var listProduct = await dbContext.Product.Include(x => x.Category).Include(x => x.Stock)
+				.Where(x => x.Name.Contains(name)).OrderByDescending(x => x.Id).ToListAsync();
 
-        public async Task addProduct(Product pro, int quan, int categoryId,int shopId, IFormFile img)
-        {
-            Stock stock = new Stock { Id = 0, Quantity = quan, CreatedDate = DateTime.Now,LastEditedDate = DateTime.Now };
-            dbContext.Stock.Add(stock);
-            var category = await dbContext.Category.FirstOrDefaultAsync(x => x.Id == categoryId);
-            var Shop = await dbContext.Shop.FirstOrDefaultAsync(x => x.Id == shopId);
-            if(pro != null)
-            {
-                pro.Image = ImgUtil.Compress(ImgUtil.ConvertIFormFileToByte(img));
-                pro.Stock = stock;
-                pro.Category = category;
-                pro.Shop = Shop;
-                dbContext.Product.Add(pro);
-                await dbContext.SaveChangesAsync();
-            }
-        }
+			listProduct.ForEach(product => { product.Image = ImgUtil.Decompress(product.Image); });
+			return listProduct;
+		}
 
-        public async Task<Product> GetProductById(int id )
-        {
-            var pro = await dbContext.Product.Include(x=>x.Category).Include(x=> x.Shop).Include(x=>x.Stock).FirstOrDefaultAsync(x => x.Id == id);
-            if (pro != null)
-            {
-                pro.Image = ImgUtil.Decompress(pro.Image);
-                
-            }
-            return pro;
-        }
-        
-        public async Task updateProduct(Product pro,int quan,string cate, IFormFile img)
-        {
-            var product = await dbContext.Product.Include(x=>x.Stock).FirstOrDefaultAsync(x => x.Id == pro.Id);
-            var catename = await dbContext.Category.FirstOrDefaultAsync(x => x.Name.Equals(cate));
+		public async Task<Product> GetProductById(int id)
+		{
+			var pro = await dbContext.Product.Include(x => x.Category).Include(x => x.Shop).Include(x => x.Stock).FirstOrDefaultAsync(x => x.Id == id);
+			if (pro != null)
+			{
+				pro.Image = ImgUtil.Decompress(pro.Image);
 
-            product.Name = pro.Name;
-            product.Description = pro.Description;
-            product.Price = pro.Price;
-            product.Image = ImgUtil.Compress(ImgUtil.ConvertIFormFileToByte(img))??product.Image;
-            product.Category = catename;
-            product.Stock.Quantity = quan;
-            dbContext.Product.Update(product);
-            await dbContext.SaveChangesAsync();
+			}
+			return pro;
+		}
 
-        }
+		public async Task<List<Category>> GetCategory()
+		{
+			return await dbContext.Category.ToListAsync();
+		}
 
-        public async Task Delete(Product pro)
-        {
-            var product =  await dbContext.Product.FirstOrDefaultAsync(x=> x.Id==pro.Id);
-            if (product != null)
-            {
-                
-                    dbContext.Product.Remove(product);
-                
-            }
-            await dbContext.SaveChangesAsync();
-        }
-        
-        public async Task Search(string name,int CurrentPage)
-        {
-            int endpage = (int)Math.Ceiling((double) dbContext.Product.Where(x=>x.Name != null && x.Name.Contains(name)).Count()/8);
-            CurrentPage = CurrentPage < 1 ? 1 : CurrentPage > endpage ? endpage : CurrentPage;
-            
-            var product =  dbContext.Product.OrderByDescending(x => x.Id).Where(x => x.Name == null && x.Name.Contains(name)).Skip((CurrentPage-1)*6).Take(6).ToList();
+		public async Task<List<Shop>> GetShop()
+		{
+			return await dbContext.Shop.ToListAsync();
+		}
 
-         
-        }
+		public int GetCountProduct()
+		{
+			return dbContext.Product.ToList().Count;
+		}
 
-        
+		public int GetCountProductBySearchName(string Name)
+		{
+			return dbContext.Product.Where(x => x.Name.Equals(Name)).ToList().Count;
+		}
 
+		public int GetCountProductByCategoryName(string CategoryName)
+		{
+			return dbContext.Product.Include(x => x.Category).Where(x => x.Category.Name.Equals(CategoryName)).ToList().Count;
+		}
 
-    }
+		public async Task AddProduct(Product pro, int quan, int categoryId, int shopId, IFormFile img)
+		{
+			Stock stock = new Stock { Id = 0, Quantity = quan, CreatedDate = DateTime.Now, LastEditedDate = DateTime.Now };
+			dbContext.Stock.Add(stock);
+			var category = await dbContext.Category.FirstOrDefaultAsync(x => x.Id == categoryId);
+			var Shop = await dbContext.Shop.FirstOrDefaultAsync(x => x.Id == shopId);
+			if (pro != null)
+			{
+				pro.Image = ImgUtil.Compress(ImgUtil.ConvertIFormFileToByte(img));
+				pro.Stock = stock;
+				pro.Category = category;
+				pro.Shop = Shop;
+				dbContext.Product.Add(pro);
+				await dbContext.SaveChangesAsync();
+			}
+		}
+
+		
+
+		public async Task UpdateProduct(Product pro, int quan, string cate, IFormFile img)
+		{
+			var product = await dbContext.Product.Include(x => x.Stock).FirstOrDefaultAsync(x => x.Id == pro.Id);
+			var catename = await dbContext.Category.FirstOrDefaultAsync(x => x.Name.Equals(cate));
+
+			product.Name = pro.Name;
+			product.Description = pro.Description;
+			product.Price = pro.Price;
+			product.Image = ImgUtil.Compress(ImgUtil.ConvertIFormFileToByte(img)) ?? product.Image;
+			product.Category = catename;
+			product.Stock.Quantity = quan;
+			product.Stock.LastEditedDate = DateTime.Now;
+			dbContext.Product.Update(product);
+			await dbContext.SaveChangesAsync();
+
+		}
+
+		public async Task Delete(Product pro)
+		{
+			var product = await dbContext.Product.FirstOrDefaultAsync(x => x.Id == pro.Id);
+			if (product != null)
+			{
+				dbContext.Product.Remove(product);
+			}
+			await dbContext.SaveChangesAsync();
+		}
+
+		
+
+		
+
+	}
 }
