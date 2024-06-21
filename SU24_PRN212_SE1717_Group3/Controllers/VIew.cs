09ebe7dab49d89ceab237c21d161ea4d7b494124
QuiNH <qn10782@gamil.com>
2024-06-21 15:28:13 +0700

    daodien1checkout

---
+++ SU24_PRN212_SE1717_Group3/Controllers/VIew.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SU24_PRN212_SE1717_Group3.Controllers
+{
+    public class VIew : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult feedbackmanager()
+        {
+            return View();
+        }
+
+        public IActionResult overviewmangerment() { 
+        return View();
+        }
+        
+        public IActionResult overviewuser()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
---
+++ SU24_PRN212_SE1717_Group3/wwwroot/js/feedbackManager.js
@@ -0,0 +1,41 @@
+﻿
+
+
+    $(function () {
+        var includes = $('[data-include]')
+        $.each(includes, function () {
+            var file = './layout/' + $(this).data('include') + '.html'
+            $(this).load(file)
+        })
+    })
+
+    var today = new Date();
+
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    var day = today.getDate();
+    var month = today.getMonth() + 1;
+    var year = today.getFullYear();
+
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    var currentDate = day + "/" + month + "/" + year;
+    var currentTime = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("time-1").innerHTML = currentTime;
+    document.getElementById("date-1").innerHTML = currentDate;
+    document.getElementById("time-2").innerHTML = currentTime;
+    document.getElementById("date-2").innerHTML = currentDate;
+
+    function toggleTextarea(id) {
+        var reply = document.querySelector('#feedback-reply-' + id);
+        if (reply.style.display == 'none') {
+            reply.style.display = 'block';
+
+        } else {
+            reply.style.display = 'none';
+        }
+    }
